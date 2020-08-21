4a9e6a5b14997c4eb71fe32388821f19633bd1fd
Slulupzzz <slulupzzz@MacBook-Pro.local>
2020-08-21 17:33:59 +0700

    initial xcode project creaetion

---
+++ youtube-onedaybuild-chris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild-chris
+//
+//  Created by Slulupzzz on 8/21/20.
+//  Copyright © 2020 Slulupzzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
